b58fdd489023c716c591a039116bc8f3204bfee4
Namide <damien@doussaud.fr>
2025-05-26 12:07:25 +0200

    Fix gpuTier

--- src/components/ThreeD.vue
+++ src/components/ThreeD.vue
@@ -46,7 +46,7 @@ async function init3D(canvas: HTMLCanvasElement) {
   const gpuTier = await getGPUTier({
     benchmarksURL: `${import.meta.env.BASE_URL}benchmarks`,
   });
-  const hq = gpuTier.tier > 3;
+  const hq = gpuTier.tier > 2;
   chessScene = new ChessScene({ canvas, hq, onReady: () => isReady.value = true })
 }
 
